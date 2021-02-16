a82859b7ffed1185c7a4862cc52b9921fed7b612
JChaBu <jchabur.1996@gmail.com>
2021-02-16 18:45:26 +0000

    Implementación de botón búsqueda funcional

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const urlTiendas = 'https://webapp-210130211157.azurewebsites.net/webresources/mitienda/';
 const contenedorMain = document.body.children[1];
-
 /**
  * @param {string} name
  *  @param {string} content
@@ -31,8 +30,12 @@ function createNode(name, content, classes, attributes) {
  * Función para añadir un div con el loader
  * @param {*} contenedor
  */
-function startLoader(contenedor) {
-    contenedor.appendChild(createNode('div', '', ['loader'], []));
+function startLoader(contenedor, tipoLoader) {
+    if (tipoLoader === 'loaderMini') {
+        contenedor.appendChild(createNode('div', '', ['loaderMini'], []));
+    } else {
+        contenedor.appendChild(createNode('div', '', ['loader'], []));
+    }
 }
 /**
  * Función para quitar loader donde sea
@@ -50,10 +53,12 @@ function imprimirForma() {
     const btnNuevaTienda = createNode('button', 'Nueva Tienda', ['boton'], []);
 
     contenedorMain.lastChild.lastChild.appendChild(btnNuevaTienda);
-    contenedorMain.lastChild.lastChild.appendChild(createNode('form', '', ['buscador'], []));
-    contenedorMain.lastChild.lastChild.lastChild.appendChild(createNode('input', '', ['barraBusqueda'], [{ name: 'type', value: 'text' }]));
-    contenedorMain.lastChild.lastChild.lastChild.appendChild(createNode('button', '', ['botonBusqueda'], [{ name: 'type', valuae: 'submit' }]));
-    contenedorMain.lastChild.lastChild.lastChild.lastChild.appendChild(createNode('i', '', ['fas', 'fa-search'], [{}]));
+    contenedorMain.lastChild.lastChild.appendChild(createNode('div', '', ['buscador'], []));
+    const barraBusqueda = createNode('input', '', ['barraBusqueda'], [{ name: 'type', value: 'text' }]);
+    contenedorMain.lastChild.lastChild.lastChild.appendChild(barraBusqueda);
+    const botonBusqueda = createNode('button', '', ['botonBusqueda'], []);
+    contenedorMain.lastChild.lastChild.lastChild.appendChild(botonBusqueda);
+    contenedorMain.lastChild.lastChild.lastChild.lastChild.appendChild(createNode('i', '', ['fas', 'fa-search'], []));
     contenedorMain.lastChild.appendChild(createNode('div', '', ['oculto', 'contNuevaTienda'], []));
     contenedorMain.lastChild.lastChild.appendChild(createNode('form', '', ['formulario'], []));
     contenedorMain.lastChild.lastChild.lastChild.appendChild(createNode('label', 'Nombre de tienda', [], [{ name: 'for', value: 'nombreTienda' }]));
@@ -63,7 +68,7 @@ function imprimirForma() {
     contenedorMain.lastChild.lastChild.lastChild.appendChild(createNode('label', 'Localidad', [], [{ name: 'for', value: 'localidadTienda' }]));
     contenedorMain.lastChild.lastChild.lastChild.lastChild.appendChild(createNode('input', '', [], [{ name: 'name', value: 'localidadTienda' }, { name: 'required', value: '' }]));
     contenedorMain.lastChild.lastChild.lastChild.appendChild(createNode('label', 'Teléfono', [], [{ name: 'for', value: 'telefonoTienda' }]));
-    contenedorMain.lastChild.lastChild.lastChild.lastChild.appendChild(createNode('input', '', [], [{ name: 'name', value: 'telefonoTienda' }, { name: 'required', value: '' }]));
+    contenedorMain.lastChild.lastChild.lastChild.lastChild.appendChild(createNode('input', '', [], [{ name: 'name', value: 'telefonoTienda' }, { name: 'required', value: '' }, { name: 'pattern', value: '[0-9]{9}' }]));
     contenedorMain.lastChild.lastChild.appendChild(createNode('button', 'Crear tienda', ['botonNuevaTienda', 'boton'], []));
     btnNuevaTienda.addEventListener('click', () => {
         if (document.getElementsByClassName('contNuevaTienda')[0].classList.contains('oculto')) {
@@ -72,18 +77,25 @@ function imprimirForma() {
             document.getElementsByClassName('contNuevaTienda')[0].classList.add('oculto');
         }
     });
-    contenedorMain.appendChild(createNode('div', '', ['listado'], []));
 }
 /**
  * @param {JSON} listaTiendas
  */
 function printLista(listaTiendas) {
-    listaTiendas.forEach((tienda) => {
+    contenedorMain.appendChild(createNode('div', '', ['listado'], []));
+    if (listaTiendas.length === undefined) {
         contenedorMain.lastChild.appendChild(createNode('div', '', ['tarjeta'], []));
-        contenedorMain.lastChild.lastChild.appendChild(createNode('h2', tienda.nombreTienda, [], []));
-        contenedorMain.lastChild.lastChild.appendChild(createNode('h3', tienda.direccion, [], []));
-        contenedorMain.lastChild.lastChild.appendChild(createNode('h3', `${tienda.localidad} - ${tienda.telefono}`, [], []));
-    });
+        contenedorMain.lastChild.lastChild.appendChild(createNode('h2', listaTiendas.nombreTienda, [], []));
+        contenedorMain.lastChild.lastChild.appendChild(createNode('h3', listaTiendas.direccion, [], []));
+        contenedorMain.lastChild.lastChild.appendChild(createNode('h2', `${listaTiendas.localidad} - ${listaTiendas.telefono}`, [], []));
+    } else {
+        listaTiendas.forEach((tienda) => {
+            contenedorMain.lastChild.appendChild(createNode('div', '', ['tarjeta'], []));
+            contenedorMain.lastChild.lastChild.appendChild(createNode('h2', tienda.nombreTienda, [], []));
+            contenedorMain.lastChild.lastChild.appendChild(createNode('h3', tienda.direccion, [], []));
+            contenedorMain.lastChild.lastChild.appendChild(createNode('h3', `${tienda.localidad} - ${tienda.telefono}`, [], []));
+        });
+    }
 }
 /**
  * Función para limpiar el main
@@ -91,13 +103,90 @@ function printLista(listaTiendas) {
 function limpiarMain() {
     contenedorMain.removeChild(contenedorMain.lastChild);
 }
+/**
+ * Función para limpiar solo el contenedor Lista
+ */
+function limpiarLista() {
+    contenedorMain.lastChild.removeChild(contenedorMain.lastChild.lastChild);
+}
 
+function cambiarBotonBusqueda(tipo) {
+    const contenedorBusqueda = document.getElementsByClassName('buscador')[0];
+    contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+    const botonNuevoBusqueda = createNode('button', '', ['botonBusqueda'], []);
+    botonNuevoBusqueda.appendChild(createNode('i', '', ['fa', 'fa-times'], []));
+    contenedorBusqueda.appendChild(botonNuevoBusqueda);
+    if (tipo === 'JQuery') {
+        botonNuevoBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            startLoader(contenedorMain, '');
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.appendChild(createNode('input', '', ['barraBusqueda'], [{ name: 'type', value: 'text' }]));
+            const botonBusqueda = createNode('button', '', ['botonBusqueda'], []);
+            contenedorBusqueda.appendChild(botonBusqueda);
+            botonBusqueda.appendChild(createNode('i', '', ['fas', 'fa-search'], []));
+            listaXHR('');
+        });
+    } else if (tipo === 'Fetch') {
+        botonNuevoBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            startLoader(contenedorMain, '');
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.appendChild(createNode('input', '', ['barraBusqueda'], [{ name: 'type', value: 'text' }]));
+            const botonBusqueda = createNode('button', '', ['botonBusqueda'], []);
+            contenedorBusqueda.appendChild(botonBusqueda);
+            botonBusqueda.appendChild(createNode('i', '', ['fas', 'fa-search'], []));
+            listaFetch('');
+        });
+    } else if (tipo === 'XHR') {
+        botonNuevoBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            startLoader(contenedorMain, '');
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.removeChild(contenedorBusqueda.lastChild);
+            contenedorBusqueda.appendChild(createNode('input', '', ['barraBusqueda'], [{ name: 'type', value: 'text' }]));
+            const botonBusqueda = createNode('button', '', ['botonBusqueda'], []);
+            contenedorBusqueda.appendChild(botonBusqueda);
+            botonBusqueda.appendChild(createNode('i', '', ['fas', 'fa-search'], []));
+            listaXHR('');
+        });
+    }
+}
+/**
+ * Busqueda con su propio tipo reutilizando funciones
+ * @param {string} tipo
+ */
+function eventoBusqueda(tipo) {
+    const botonBusqueda = document.getElementsByClassName('botonBusqueda')[0];
+    const inputBusqueda = document.getElementsByClassName('barraBusqueda')[0];
+    if (tipo === 'XHR') {
+        botonBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            listaXHR(inputBusqueda.value);
+            cambiarBotonBusqueda('XHR');
+        });
+    } else if (tipo === 'Fetch') {
+        botonBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            listaFetch(inputBusqueda.value);
+            cambiarBotonBusqueda('Fetch');
+        });
+    } else if (tipo === 'JQuery') {
+        botonBusqueda.addEventListener('click', () => {
+            limpiarLista();
+            listaJQuery(inputBusqueda.value);
+            cambiarBotonBusqueda('JQuery');
+        });
+    }
+}
 /**
  * Función para sacar la lista de tiendas automaticamente en XHRHttpRequest
  */
-function listaXHR() {
+function listaXHR(terminacion) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', urlTiendas);
+    xhr.open('GET', urlTiendas + String(terminacion));
     xhr.responseType = 'json';
     xhr.send();
 
@@ -106,6 +195,7 @@ function listaXHR() {
             const data = this.response;
             stopLoader();
             printLista(data);
+            eventoBusqueda('XHR');
         } catch (err) {
             console.error(err);
         }
@@ -115,24 +205,25 @@ function listaXHR() {
 /**
  * Función para sacar la lista de tiendas en Fetch
  */
-function listaFetch() {
-    fetch(urlTiendas)
+function listaFetch(terminacion) {
+    fetch(urlTiendas + terminacion)
         .then((response) => response.json())
-        .then((data) => { stopLoader(); printLista(data); })
+        .then((data) => { stopLoader(); printLista(data); eventoBusqueda('Fetch'); })
         .catch((error) => console.log(error));
 }
 
 /**
  * Función para sacar la lista de tiendas en JQuery
  */
-function listaJQuery() {
+function listaJQuery(terminacion) {
     $.ajax({
-        url: urlTiendas,
+        url: urlTiendas + terminacion,
         type: 'GET',
         dataType: 'json',
         success(json) {
             stopLoader();
             printLista(json);
+            eventoBusqueda('JQuery');
         },
         error() {
             console.log('Disculpe, existió un problema');
@@ -146,9 +237,9 @@ function listaJQuery() {
  */
 function tiendaXHR() {
     limpiarMain();
-    imprimirForma('XHR');
-    startLoader(contenedorMain);
-    listaXHR();
+    imprimirForma();
+    startLoader(contenedorMain, '');
+    listaXHR('');
 }
 /**
  * Función para invocar el formato y todo lo necesario para el funcionamiento en Fetch
@@ -156,8 +247,8 @@ function tiendaXHR() {
 function tiendaFetch() {
     limpiarMain();
     imprimirForma('Fetch');
-    startLoader(contenedorMain);
-    listaFetch();
+    startLoader(contenedorMain, '');
+    listaFetch('');
 }
 /**
  * Función para invocar el formato y todo lo necesario para el funcionamiento en jQuery
@@ -165,8 +256,8 @@ function tiendaFetch() {
 function tiendaJQuery() {
     limpiarMain();
     imprimirForma('JQuery');
-    startLoader(contenedorMain);
-    listaJQuery();
+    startLoader(contenedorMain, '');
+    listaJQuery('');
 }
 /**
  * Función para crear la vista principal de la página donde seleccionas el tipo de petición AJAX
